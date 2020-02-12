7894066ea3b7c0e03933a3c03e3ba91fd680f5be
dinaolmelak <dinaolmelak@gmail.com>
2020-02-11 19:26:32 -0600

    Initial Commit

---
+++ tipcalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipcalc
+//
+//  Created by Dinaol Melak on 2/11/20.
+//  Copyright © 2020 Dinaol Melak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
